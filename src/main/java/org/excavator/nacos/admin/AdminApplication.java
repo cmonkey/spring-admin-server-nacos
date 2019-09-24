19e3f1a385e9682cbc159b0fce0e54117f845663
CMonkey <42.codemonkey@gmail.com>
2019-09-24 14:37:04 +0800

    add enable admin server

--- src/main/java/org/excavator/nacos/admin/AdminApplication.java
+++ src/main/java/org/excavator/nacos/admin/AdminApplication.java
@@ -1,9 +1,13 @@
 package org.excavator.nacos.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableAdminServer
 public class AdminApplication {
 
 	public static void main(String[] args) {
